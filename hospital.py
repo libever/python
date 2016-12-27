ab34db0dfb22b79064b8767cd83b8013c25948c8
xingyue <qixingyue@126.com>
2016-12-27 08:56:02 +0800

    获取北京医保的定点医院

---
+++ hospital.py
@@ -0,0 +1,33 @@
+#!/bin/env python 
+#coding=utf-8
+
+import requests
+from pyquery import PyQuery as PQ
+
+
+def do_url(url):
+    f_find = 0
+    content = requests.get(url).text
+    
+    dom = PQ(content)
+    
+    rows = dom.find("table[bgcolor]").find("tr")
+    
+    for row in rows:
+        cells = PQ(row).find("td")
+        if len(cells) == 5 :
+            if f_find == 0 :
+                f_find = 1
+            else :
+                line = ""
+                for cell in cells:
+                    t = PQ(cell).text().strip()
+                    line = line + t + ","
+    
+                print (line + ";").encode("gbk")
+    
+for sno in range(0,105):
+    url = "http://www.bjrbj.gov.cn/LDJAPP/search/ddyy/ddyy_01_outline_new.jsp?sno=%d&spage=0&epage=105&leibie=&suoshu=&sword=" % (sno * 20)
+    do_url(url)
+
+
