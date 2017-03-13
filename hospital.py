79fa52a4fb61118fe5387d65bffb1d0c51981daa
xingyue <qixingyue@126.com>
2017-03-13 15:46:55 +0800

    医保医院列表

--- hospital.py
+++ hospital.py
@@ -4,6 +4,11 @@
 import requests
 from pyquery import PyQuery as PQ
 
+"""
+获取北京社保的定点医院!
+希望你们永远用不到这个文件.
+"""
+
 
 def do_url(url):
     f_find = 0
@@ -24,7 +29,7 @@ def do_url(url):
                     t = PQ(cell).text().strip()
                     line = line + t + ","
     
-                print (line + ";").encode("gbk")
+                print line
     
 for sno in range(0,105):
     url = "http://www.bjrbj.gov.cn/LDJAPP/search/ddyy/ddyy_01_outline_new.jsp?sno=%d&spage=0&epage=105&leibie=&suoshu=&sword=" % (sno * 20)
